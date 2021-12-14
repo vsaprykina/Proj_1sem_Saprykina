a1fd82dcbecd181e09d3d2ddf301446e601e3854
Student <astamirova_elizaveta@mail.ru>
2021-12-14 17:23:01 +0300

    pz_6

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,18 +1,8 @@
-#Дан список размера N, все элементы которого, кроме последнего, упорядочены по возрастанию.
+# Дан список размера N, все элементы которого, кроме последнего, упорядочены по возрастанию.
 # Сделать список упорядоченным, переместив последний элемент на новую позицию.
-import random
-N = []
-k = 1
-n = input('Введите размер списка: ')
-while type(n) != int:
-    try:
-        n = int(n)
-    except ValueError:
-        n = input('Это не число!!! Попробуйте снова: ')
-for x in range(n):
-    N.append(random.randint(0, n))
-N.sort()
-N.insert(random.randint(0, len(N)), N[-1])
-N.pop(-1)
-N.sort()
-print('Упорядоченный список: ', N)
+spisok = []
+for i in range(10):#10 - длина списка
+	spisok.append(i)
+spisok.append(5)
+spisok.sort()
+print(spisok)
