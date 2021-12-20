e85e0ec803c820f85d547d4f3c719a140ae44129
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2021-12-20 19:40:55 +0300

    Update PZ_6_2.py

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,18 +1,24 @@
 #Дан целочисленный список размера N. Найти количество различных элементов в данном списке.
 
 from random import randint
-dlina_spiska = int(input("Введите длину списка:"))
-spisok = []
-for i in range(dlina_spiska):
-    spisok.append(randint(-10, 10))
-print("Список:", end = ' ')
-for i in range(dlina_spiska):
-	print(spisok[i],end=';')
-print()
 
-k = 0
-for i in range(dlina_spiska):
-	if not spisok[i] in spisok[0:i]:
-		k += 1
-print(k)
+
+n = int(input('Введите количество элементов: '))
+List = []
+
+count = 0  # заполнение списка
+while count < n:
+    count += 1
+    List.append(randint(0, 2))
+
+print(List)
+
+List_unic = []  # Список для элементов без повтора
+
+for element in List:
+    if List.count(element) == 1:
+        List_unic.append(element)
+
+print('Количество уникальных элементов: ',
+      len(List_unic))
 
