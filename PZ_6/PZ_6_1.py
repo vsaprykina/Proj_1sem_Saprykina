f5bd7adaad60020f38658fee212642d0a1e76c82
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2021-12-20 19:28:58 +0300

    Update PZ_6_1.py

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,34 +1,25 @@
 # Дан список размера N, и целые числа К и L(1<K<L<N). Найти сумму элементов списка с номерами от K до L включительно.
 
-import random
-n = input('Введите размер списка: ')
-while type(n) != int:
-    try:
-        n = int(n)
-    except ValueError:
-        n = input('Это не число!!! Попробуйте снова: ')
-if n == 1:
-    print('Сумму элементов списка с номерами от K до L включительно: ', 1)
-elif n == 2:
-    N = []
-    for x in range(2):
-        N.append(random.randint(1, n))
-    print('Сумму элементов списка с номерами от K до L включительно: ', sum(N))
-elif n>2:
-    N = []
-    for x in range(n):
-        N.append(random.randint(1, n))
-    L = random.randrange(1, n)
-    while L == 1:
-        L = random.randrange(1, n)
-    K = random.randrange(1, L)
-    summa = 0
-    for x in N:
-        if K <= x <= L:
-            summa += x
-        else:
-            ...
-    print('Сумму элементов списка с номерами от K до L включительно: ', summa)
+from random import randint
+
+n = int(input('Введите размер списка: '))
+List = []
+a = 0
+while a < n: #заполнение списка
+    a += 1
+    List.append(randint(-10, 10))
+print('Ваш список: ', List)
+
+if n < 3:
+    print('Сумма элементов списка: ',
+sum(List))
+else:
+    k = randint(2, n)
+    I = randint(2, n)
+    if k > I:
+        k, I = I, k
+    print(f'Сумма элементов списка с номерами от {k} до {I} включительно: ',
+sum(List[k:I]))
 
 
 
