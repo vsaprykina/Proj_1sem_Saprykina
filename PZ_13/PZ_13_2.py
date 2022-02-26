b6910ae6bd7020c7f69681f0907114cd609e1ae9
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2022-02-26 17:23:42 +0300

    Update PZ_13_2.py

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,1 +1,8 @@
+# Составить генератор (yield), который преобразует все буквенные символы в заглавные.
+def finding_letters(n):
+    yield from [i.upper() for i in n]
+
+letters = input('Введите буквы в нижнем регистре: ')
+print(letters)
+print(''.join([o for o in finding_letters(letters)]))
 
