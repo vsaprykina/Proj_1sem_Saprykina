9df75ae0dd806f5423e9c773b11223a3ba5c7396
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2022-04-12 21:30:32 +0300

    Update code.py

--- figures/triangle/code.py
+++ figures/triangle/code.py
@@ -1,11 +1,8 @@
-import math
-__all__ = ['triangle_perimeter', 'triangle_area']
-a, b, c = 7, 2, 8
-
-
 def triangle_perimeter(a=7, b=2, c=8):
     return a + b + c
 
-
 def triangle_area(a=7, b=2, c=8):
-    return math.sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c))
+    from math import sqrt
+    p = a + b + c
+    h = (2 * sqrt(p * (p - a) * (p - b) * (p - c)))/ a
+    return a / 2 * h
